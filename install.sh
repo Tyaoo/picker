e057733884db2aaa31b08e63fd95aa52d7da319f
Tyaoo <39017274+Tyaoo@users.noreply.github.com>
2023-04-03 19:28:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 python3 -m pip install -r requirements.txt
 
 sudo apt-get install -y wget
+mkdir ./cqhttp
 wget https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0/go-cqhttp_darwin_amd64.tar.gz -O ./cqhttp/go-cqhttp.tar.gz
 cd cqhttp && tar xzf go-cqhttp.tar.gz go-cqhttp && rm go-cqhttp.tar.gz
